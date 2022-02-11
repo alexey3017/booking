12653c5166c314703633f7386a025a0200220199
Aleksei <alexey3017@mail.ru>
2022-02-11 16:51:21 +0300

    initial commit frontend and backend

---
+++ client/src/booking/Home.js
@@ -0,0 +1,8 @@
+import { userSelector, useSelector, useStore } from 'react-redux';
+
+const Home = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+  return <div className="container-fluid h1 p-5 text-center">Home Page </div>;
+};
+
+export default Home;
